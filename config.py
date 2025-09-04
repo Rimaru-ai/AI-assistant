0f39361d5930c97f98f23450c9adf4dd978de12e
Sayan B <sayan92luf@gmail.com>
2025-09-04 18:12:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 # Toggle between free and pro
-USE_PRO_MODE = False  # False = Free Mode (local), True = Pro Mode (OpenAI)
+USE_PRO_MODE = True  # False = Free Mode (local), True = Pro Mode (OpenAI)
